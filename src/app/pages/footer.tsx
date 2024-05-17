6d269b52804b9050832a8b1da1b0d5cf3e562171
Ramiro Alejandro Alvarez <ramiro.al.alvarez@gmail.com>
2024-05-17 00:10:39 -0300

    hecho: lets talk section

--- src/app/pages/footer.tsx
+++ src/app/pages/footer.tsx
@@ -5,19 +5,15 @@ import LinksColumn from "../components/LinksColumn";
 import Link from "next/link";
 import Image from "next/image";
 import arrowUp from "../../../public/icons/arrow-up.svg";
-import { useRef } from "react";
 
 interface Props {}
 
+interface LinksColumnStyles {
+  tailwindParentProperties: { properties: string };
+  tailwindListElementProperties: { properties: string };
+}
+
 const Footer: NextPage<Props> = ({}) => {
-  interface LinksColumnStyles {
-    tailwindParentProperties: {
-      properties: string;
-    };
-    tailwindListElementProperties: {
-      properties: string;
-    };
-  }
   const LinksColumnStyles: LinksColumnStyles = {
     tailwindParentProperties: {
       properties:
@@ -31,7 +27,7 @@ const Footer: NextPage<Props> = ({}) => {
   return (
     <>
       <footer id="footer" className="tw-flex tw-flex-col tw-justify-end">
-        <div className="footer__container tw-h-3/4 tw-w-full tw-flex tw-flex-row tw-mb-12 tw-mt-12">
+        <div className="footer__container tw-h-3/4 tw-w-full tw-flex tw-flex-row tw-mb-12 tw-mt-12 ">
           <Logo
             svgImage={logoFooter}
             height={60}
@@ -40,77 +36,81 @@ const Footer: NextPage<Props> = ({}) => {
             tailwindParentProperties="tw-h-full tw-w-1/5 tw-flex tw-flex-col tw-justify-start tw"
             tailwindImgProperties="tw-ml-4 "
           />
-        
-        <div className="links__container tw-w-3/5 tw-flex tw-flex-row">
-          {/*First column*/}
-          <LinksColumn
-            links={[
-              { text: "Contacto", url: "https://www.google.com" },
-              {
-                text: "Barcelona,Spain Lorem, ipsum dolor sit amet consectetur adipisicing",
-                url: "https://www.google.com",
-              },
-            ]}
-            tailwindParentProperties={
-              LinksColumnStyles.tailwindParentProperties.properties
-            }
-            tailwindListElementProperties={
-              LinksColumnStyles.tailwindListElementProperties.properties
-            }
-            tailwindAnchorTagProperies=""
-          />
-          {/*Second column*/}
-          <LinksColumn
-            links={[
-              { text: "General", url: "https://www.google.com" },
-              {
-                text: "Terms&Conditions",
-                url: "https://www.google.com",
-              },
-              {
-                text: "Privacy Policy",
-                url: "https://www.google.com",
-              },
-              {
-                text: "Cookies",
-                url: "https://www.google.com",
-              },
-            ]}
-            tailwindParentProperties={
-              LinksColumnStyles.tailwindParentProperties.properties
-            }
-            tailwindListElementProperties={
-              LinksColumnStyles.tailwindListElementProperties.properties
-            }
-            tailwindAnchorTagProperies=""
-          />
-          {/*Third column*/}
-          <LinksColumn
-            links={[
-              { text: "About", url: "https://www.google.com" },
-              {
-                text: "work",
-                url: "https://www.google.com",
-              },
-              {
-                text: "Contact",
-                url: "https://www.google.com",
-              },
-            ]}
-            tailwindParentProperties={
-              LinksColumnStyles.tailwindParentProperties.properties
-            }
-            tailwindListElementProperties={
-              LinksColumnStyles.tailwindListElementProperties.properties
-            }
-            tailwindAnchorTagProperies=""
-          />
+
+          <div className="links__container tw-w-3/5 tw-flex tw-flex-row">
+            {/*First column*/}
+            <LinksColumn
+              links={[
+                { text: "Contacto", url: "https://www.google.com" },
+                {
+                  text: "Barcelona,Spain Lorem, ipsum dolor sit amet consectetur adipisicing",
+                  url: "https://www.google.com",
+                },
+              ]}
+              tailwindParentProperties={
+                LinksColumnStyles.tailwindParentProperties.properties
+              }
+              tailwindListElementProperties={
+                LinksColumnStyles.tailwindListElementProperties.properties
+              }
+              tailwindAnchorTagProperies=""
+            />
+            {/*Second column*/}
+            <LinksColumn
+              links={[
+                { text: "General", url: "https://www.google.com" },
+                {
+                  text: "Terms&Conditions",
+                  url: "https://www.google.com",
+                },
+                {
+                  text: "Privacy Policy",
+                  url: "https://www.google.com",
+                },
+                {
+                  text: "Cookies",
+                  url: "https://www.google.com",
+                },
+              ]}
+              tailwindParentProperties={
+                LinksColumnStyles.tailwindParentProperties.properties
+              }
+              tailwindListElementProperties={
+                LinksColumnStyles.tailwindListElementProperties.properties
+              }
+              tailwindAnchorTagProperies=""
+            />
+            {/*Third column*/}
+            <LinksColumn
+              links={[
+                { text: "About", url: "https://www.google.com" },
+                {
+                  text: "work",
+                  url: "https://www.google.com",
+                },
+                {
+                  text: "Contact",
+                  url: "https://www.google.com",
+                },
+              ]}
+              tailwindParentProperties={
+                LinksColumnStyles.tailwindParentProperties.properties
+              }
+              tailwindListElementProperties={
+                LinksColumnStyles.tailwindListElementProperties.properties
+              }
+              tailwindAnchorTagProperies=""
+            />
           </div>
 
-          <div className="upArrowContainer tw-w-1/5 tw-flex tw-flex-row tw-items-start tw-justify-center">
-            <Link href="/">UP</Link>
-            <Image src={arrowUp} width={30} height={30} alt="up arrow" />
+          <div className="upArrowContainer tw-pt-20 tw-w-1/5 tw-flex tw-flex-row tw-items-end tw-justify-end">
+            
+              <Link href="/">UP</Link>
+              <Image src={arrowUp} width={30} height={30} alt="up arrow" />
+            
           </div>
+
+
         </div>
       </footer>
     </>
