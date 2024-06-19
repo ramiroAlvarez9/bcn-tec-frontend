b059c3005d29a9b7e3535a5d478b781c17730b1e
Ramiro Alvarez <ramiro.al.alvarez@gmail.com>
2024-06-18 23:05:13 -0300

    rebuild

--- src/app/idHookPages/projects.tsx
+++ src/app/idHookPages/projects.tsx
@@ -5,17 +5,15 @@ const Projects = async () => {
 
   const spaceId = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID;
   const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN;
-  
+
   const response = await getProjectsFromContentful(spaceId,accessToken,"proyectos");
 
   return (
     <>
       <main id="projects">
-      
       < Carousel 
         proyectos={response}
       />
-
       </main>
     </>
   );
