74495f8b4b43156e8a444cb3b9e000c82c70202d
Ramiro Alejandro Alvarez <ramiro.al.alvarez@gmail.com>
2024-06-18 21:36:23 -0300

    projects in pages/

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import MainPage from "./idHookPages/main";
 import About from "./idHookPages/about";
-import Projects from "./idHookPages/projects";
+import Projects from "../../pages/projects";
 import LetsTalk from "./idHookPages/lets-talk";
 import FooterSpace from "./components/FooterSpace";
 import FooterUpArrow from "./components/FooterUpArrow";
